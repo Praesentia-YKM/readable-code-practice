e0a399216910adc1b72046e6e49007ceaaea0f88
Praesentia-YKM <68469694+Praesentia-YKM@users.noreply.github.com>
2024-10-10 23:54:19 +0900

    Tutor's Refactor(1) - 추상화 레벨 맞추기 - 중복제거 - 객체에 메시지보내기

---
+++ src/main/java/cleancode/studycafe/tutorsTobe/StudyCafeApplication.java
@@ -0,0 +1,12 @@
+package cleancode.studycafe.tutorsTobe;
+
+import cleancode.studycafe.tutorsTobe.StudyCafePassMachine;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
---
+++ src/main/java/cleancode/studycafe/tutorsTobe/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.tutorsTobe.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
